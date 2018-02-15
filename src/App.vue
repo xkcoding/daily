2757fd900dcbea5be7e29eb19be70f957ab0ebc4
Yangkai.Shen <237497819@qq.com>
2018-02-15 21:42:40 +0800

    修复切换菜单的时候 bug

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,7 @@ export default {
     },
     handleToRecommend () {
       this.type = 'recommend'
-      this.showThemes = !this.showThemes
+      this.showThemes = false
       this.recommendList = []
       this.dailyTime = $.getTodayTime()
       this.getRecommendList()
